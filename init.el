852ba74c82d3e1ae8dac2ab7e0c612beb1926986
Xiu Di <akaeecs@163.com>
2023-02-06 22:57:22 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,58 @@
+;; mirror
+;; @source https://mirrors.tuna.tsinghua.edu.cn/help/elpa/
+(require 'package)
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+;; use-package
+;; M-x package-install RET use-package
+(eval-when-compile
+  (require 'use-package))
+
+;; doom-themes
+;; @source https://github.com/doomemacs/themes#manually--use-package
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
+	doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-acario-dark t)
+  (doom-themes-treemacs-config))
+
+;; all-the-icons
+;; @source https://github.com/domtronn/all-the-icons.el#installation
+;; On Windows, after downloading the fonts, one needs to manually install them
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+;; treemacs for workspace management
+;; depend on all-the-icons
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (treemacs-tag-follow-mode)
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ;; ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag))
+  (:map treemacs-mode-map
+	("/" . treemacs-advanced-helpful-hydra)))
+
+(use-package treemacs-projectile
+  :ensure t
+  :after (treemacs projectile))
+
+(use-package lsp-treemacs
+  :ensure t
+  :after (treemacs lsp))
+
+(provide 'init)
+;;; init.el ends here
