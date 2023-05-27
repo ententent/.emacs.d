ff29030aa003bf363255bcf07675dbb36992f2f4
Di Xiu <akaeecs@163.com>
2023-05-27 12:40:08 +0800

    ADD sdcv and corresponding dictionaries

--- lisp/init-english.el
+++ lisp/init-english.el
@@ -3,6 +3,32 @@
 
 ;;; Code:
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/sdcv/")
+(require 'sdcv)
+
+(use-package sdcv
+  :commands (sdcv-search-pointer+)
+  :bind
+  (("\e\e ," . sdcv-search-pointer+)
+   ("\e\e ." . sdcv-search-pointer))
+  :config
+  (setq sdcv-say-word-p t)
+  (setq sdcv-dictionary-data-dir (expand-file-name "~/.emacs.d/site-lisp/sdcv"))
+  (setq sdcv-dictionary-simple-list
+      '(
+        "懒虫简明英汉词典"
+        "懒虫简明汉英词典"
+        ))
+  (setq sdcv-dictionary-complete-list
+      '(
+        "朗道英汉字典5.0"
+        "牛津英汉双解美化版"
+        ))
+  (setq sdcv-tooltip-timeout 10)
+  (setq sdcv-fail-notify-string "没找到释义")
+  (setq sdcv-tooltip-border-width 2)
+  )
+
 (add-to-list 'load-path "~/.emacs.d/site-lisp/popweb/")
 (setq popweb-python-command "/usr/bin/python")
 ;; Proxy
