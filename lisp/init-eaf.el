dcfd4258b91608b97fa177474f3753644d8f1475
Di Xiu <akaeecs@163.com>
2023-06-10 15:36:49 +0800

    CHANGE (pdf-tools, org-noter) into (eaf-pdf-viewer, eaf-interleave)

--- lisp/init-eaf.el
+++ lisp/init-eaf.el
@@ -3,62 +3,91 @@
 
 ;;; Code:
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
-(require 'eaf)
-;;; 配置 EAF 代理
-(setq eaf-proxy-type "http")
-(setq eaf-proxy-host "127.0.0.1")
-(setq eaf-proxy-port "7890")
-;; 配置 EAF 依赖的 Python 环境
-(setq eaf-python-command "/usr/bin/python")
+  (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+  (require 'eaf)
 
-;; eaf-browser
-(require 'eaf-browser)
-(setq eaf-browser-continue-where-left-off t)
-(setq eaf-browser-enable-adblocker t)
-(setq browse-url-browser-function 'eaf-open-browser)
+  ;;; eaf general settings
+  (setq eaf-python-command "/usr/bin/python")
 
-;; eaf-rss-reader
-;;; 一些优质RSS订阅源
-;;; https://manateelazycat.github.io/feed.xml          ;; manateeLazyCat
-;;; https://superliooon.com/feed/                      ;; 即凉
-;;; http://blindwith.science/index.xml                 ;; Blind with Science
-;;; https://remacs.cc/index.xml                        ;; remacs的世界
-;;; https://www.bmpi.dev/index.xml                     ;; BMPI
-;;; http://www.ruanyifeng.com/blog/atom.xml            ;; Ruan YiFeng
-;;; https://emacstalk.codeberg.page/podcast/index.xml  ;; EmacsTalk
-;;; https://arxiv.org/rss/cs.CV                        ;; ArXiv CV
-;;; https://sachachua.com/blog/feed/                   ;; Sacha Chua
-;;; https://byvoid.com/zhs/feed.xml                    ;; byvoid
-;;; https://www.skyue.com/feed                         ;; skyue
-;;; https://einverne.github.io/feed.xml                ;; Verne
-;;; https://liujiacai.net/atom.xml                     ;; 刘家财
-;;; https://planet.emacslife.com/                      ;; Alvaro Ramirez
-;;; https://leovan.me/index.xml                        ;; 范叶亮
-(require 'eaf-rss-reader)
+  ;;; eaf-proxy
+  (setq eaf-proxy-type "http")
+  (setq eaf-proxy-host "127.0.0.1")
+  (setq eaf-proxy-port "7890")
 
-;; eaf-git
-(require 'eaf-git)
-;; eaf-pdf-viewer
-;;; (require 'eaf-pdf-viewer)
-;; eaf-image-viewer
-(require 'eaf-image-viewer)
-;; eaf-video-player
-(require 'eaf-video-player)
-;; eaf-music-player
-(require 'eaf-music-player)
-(setq eaf-music-default-file "~/MEGA/Music/songs")
+  ;;; eaf-browser
+  (require 'eaf-browser)
+  (setq eaf-browser-continue-where-left-off t)
+  (setq eaf-browser-enable-adblocker t)
+  (setq browse-url-browser-function 'eaf-open-browser)
 
-;; eaf-pyqterminal
-(require 'eaf-pyqterminal)
-;; eaf-file-manager
-(require 'eaf-file-manager)
-;; eaf-system-monitor
-(require 'eaf-system-monitor)
-;; eaf-markdown-previewer
-(require 'eaf-markdown-previewer)
-;; eaf-org-previewer
-(require 'eaf-org-previewer)
+  ;;; eaf-rss-reader
+  ;; 一些优质RSS订阅源
+  ;; https://manateelazycat.github.io/feed.xml           manateeLazyCat
+  ;; https://superliooon.com/feed/                       即凉
+  ;; http://blindwith.science/index.xml                  Blind with Science
+  ;; https://remacs.cc/index.xml                         remacs的世界
+  ;; https://www.bmpi.dev/index.xml                      BMPI
+  ;; http://www.ruanyifeng.com/blog/atom.xml             Ruan YiFeng
+  ;; https://emacstalk.codeberg.page/podcast/index.xml   EmacsTalk
+  ;; https://arxiv.org/rss/cs.CV                         ArXiv CV
+  ;; https://sachachua.com/blog/feed/                    Sacha Chua
+  ;; https://byvoid.com/zhs/feed.xml                     byvoid
+  ;; https://www.skyue.com/feed                          skyue
+  ;; https://einverne.github.io/feed.xml                 Verne
+  ;; https://liujiacai.net/atom.xml                      刘家财
+  ;; https://planet.emacslife.com/                       Alvaro Ramirez
+  ;; https://leovan.me/index.xml                         范叶亮
+  (require 'eaf-rss-reader)
+
+  ;;; eaf-git
+  (require 'eaf-git)
+
+  ;;; eaf-pdf-viewer
+  (require 'eaf-pdf-viewer)
+  ;; 配置 eaf-interleave, 配合使用 eaf-pdf-viewer 和 org-mode
+  ;; https://github.com/emacs-eaf/emacs-application-framework/blob/master/extension/eaf-interleave.el
+  (require 'eaf-interleave)
+  (add-hook 'eaf-browser-hook 'eaf-interleave-app-mode)
+  (add-hook 'eaf-pdf-viewer-hook 'eaf-interleave-app-mode)
+  (add-hook 'org-mode-hook 'eaf-interleave-mode)
+  (with-eval-after-load 'eaf-interleave
+    (define-key eaf-interleave-app-mode-map (kbd "C-c M-o") 'eaf-interleave-open-notes-file)
+    (define-key eaf-interleave-app-mode-map (kbd "C-c M-i") 'eaf-interleave-add-note)    
+    (define-key eaf-interleave-app-mode-map (kbd "C-c M-q") 'eaf-interleave-quit)
+    (define-key eaf-interleave-mode-map (kbd "M-.") 'eaf-interleave-sync-current-note)
+    (define-key eaf-interleave-mode-map (kbd "M-p") 'eaf-interleave-sync-previous-note)
+    (define-key eaf-interleave-mode-map (kbd "M-n") 'eaf-interleave-sync-next-note))
+  ;; 默认笔记路径, 自动在该目录中寻找同名 org 文件
+  (setq eaf-interleave-org-notes-dir-list '("~/org/roam/ref"))
+  ;; 分隔文档和笔记
+  (setq eaf-interleave-split-direction 'vertical
+	eaf-interleave-split-lines 20
+	eaf-interleave-disable-narrowing t)
+
+  ;;; eaf-image-viewer
+  (require 'eaf-image-viewer)
+
+  ;;; eaf-video-player
+  (require 'eaf-video-player)
+
+  ;;; eaf-music-player
+  (require 'eaf-music-player)
+  (setq eaf-music-default-file "~/MEGA/Music/songs")
+
+  ;;; eaf-pyqterminal
+  (require 'eaf-pyqterminal)
+
+  ;;; eaf-file-manager
+  (require 'eaf-file-manager)
+
+  ;;; eaf-system-monitor
+  (require 'eaf-system-monitor)
+
+  ;;; eaf-markdown-previewer
+  (require 'eaf-markdown-previewer)
+
+  ;;; eaf-org-previewer
+  (require 'eaf-org-previewer)
 
 (provide 'init-eaf)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- lisp/init-research.el
+++ lisp/init-research.el
@@ -70,55 +70,6 @@
                 :target
                 (file+head "ref/${citekey}.org" "#+title: ${title}\n"))))
 
-(use-package pdf-tools
-  :mode ("\\.pdf\\'" . pdf-view-mode)                           ;; pdf 文件默认打开方式
-  :bind
-  (:map pdf-view-mode-map
-        ("d" . pdf-view-next-page-command)                      ;; 向后翻页
-        ("a" . pdf-view-previous-page-command)                  ;; 向前翻页
-        ("s" . pdf-view-scroll-up-or-next-page)                 ;; 向下滑动
-        ("w" . pdf-view-scroll-down-or-previous-page)           ;; 向上滑动
-   :map pdf-history-minor-mode-map
-        ("b" . pdf-history-backward)
-   :map pdf-annot-minor-mode-map
-        ("C-a a" . pdf-annot-add-highlight-markup-annotation)
-        ("C-a s" . pdf-annot-add-squiggly-markup-annotation)
-        ("C-a u" . pdf-annot-add-underline-markup-annotation)
-        ("C-a d" . pdf-annot-delete))
-  :custom
-  (pdf-view-midnight-colors '("#000000" . "#9bCD9b"))           ;; 夜间模式设置绿色底色
-  (native-comp-deferred-compilation-deny-list '(".*pdf.*"))     ;; 禁用 =pdf-tools= 有关文件的本地编译
-  :config
-  (require 'pdf-annot)                                          ;; 设置 pdf-annot-minor-mode-map
-  (require 'pdf-history)                                        ;; 设置 pdf-history-minor-mode-map
-  (add-hook 'pdf-view-mode-hook 'pdf-view-midnight-minor-mode)  ;; 默认夜间模式
-  (add-hook 'pdf-view-mode-hook 'pdf-view-fit-width-to-window)  ;; 打开PDF时自动缩放
-  (pdf-tools-install))
-
-(use-package org-noter
-  :ensure t
-  :defer t
-  :bind
-  ;; 与org-roam配合，打开org-noter的快捷键
-  (("C-c n n" . org-noter)
-   ;; 加入左手键位
-   :map org-noter-doc-mode-map
-   ("e" . org-noter-insert-note)
-   ("M-e" . org-noter-insert-precise-note))
-  :custom
-  ;; 选中文字后插入笔记自动高亮
-  (org-noter-highlight-selected-text t)
-  ;; 默认笔记路径。设置后从 pdf 文件中使用 =org-noter= 命令，会自动在该墓中寻找与文件同名的 =.org= 笔记文件
-  (org-noter-notes-search-path '("~/org/roam/ref/"))
-  ;; 将文档和笔记分割为两个大小相等的窗口
-  (org-noter-doc-split-fraction '(0.5 . 0.5))
-  ;; 自动保存上次阅读位置
-  (org-noter-auto-save-last-location t)
-  ;; 修改长/短文本标准，默认为80
-  (org-noter-max-short-selected-text-length 20)
-  ;; 默认短标题格式
-  (org-noter-default-heading-title "第 $p$ 页的笔记"))
-
 ;; 对于Windows系统，需要安装ImageMagick，并保证magick.exe在PATH变量的路径中
 ;;; 用msys2安装: pacman -S mingw-w64-x86_64-imagemagick
 (use-package org-download
@@ -134,41 +85,42 @@
   ;; 用同级 ./img 目录放置截图文件
   (setq-default org-download-image-dir "./img"))
 
-;; 打开 TeX 文件时应执行的命令
-(defun my-latex-hook ()
-  (turn-on-cdlatex)          ;; 加载cdlatex
-  (turn-on-reftex)           ;; 加载reftex
-  (prettify-symbols-mode t)  ;; 加载prettify-symbols-mode
-  (outline-minor-mode)       ;; 加载outline-mode
-  (outline-hide-body))       ;; 打开文件时只显示章节标题
+  ;; 打开 TeX 文件时应执行的命令
+  (defun my-latex-hook ()
+    (turn-on-cdlatex)          ;; 加载cdlatex
+    (turn-on-reftex)           ;; 加载reftex
+    (prettify-symbols-mode t)  ;; 加载prettify-symbols-mode
+    (outline-minor-mode)       ;; 加载outline-mode
+    (outline-hide-body))       ;; 打开文件时只显示章节标题
 
-;; tex
-;; @source https://github.com/jwiegley/use-package/issues/379#issuecomment-246161500
-(use-package tex
-  :defer t
-  :ensure auctex
-  :custom
-  ;; 对新文件自动解析 (usepackage, bibliograph, newtheorem) 等信息
-  (TeX-parse-selt t)
-  (TeX-PDF-mode t)
-  ;; 正向与反向搜索设置
-  (TeX-source-correlate-mode t)
-  (TeX-source-correlate-method 'synctex)
-  ;; 使用pdf-tools打开PDF
-  (TeX-view-program-selection '((output-pdf "PDF Tools")))
-  :config
-  (setq TeX-auto-save t)
-  ;; 编译时问询主文件名称
-  (setq-default TeX-master t)                               
-  ;; 编译后更新pdf文件
-  (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
-  ;; 加载LaTeX模式设置
-  (add-hook 'LaTeX-mode-hook 'my-latex-hook)
-  ;; XeLaTeX 支持
-  (add-hook 'LaTeX-mode-hook (lambda()
-  (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
-  (setq TeX-save-query  nil )
-  (setq TeX-show-compilation t))))
+  ;; tex
+  ;; @source https://github.com/jwiegley/use-package/issues/379#issuecomment-246161500
+  (use-package tex
+    :defer t
+    :ensure auctex
+    :custom
+    ;; 对新文件自动解析 (usepackage, bibliograph, newtheorem) 等信息
+    (TeX-parse-selt t)
+    (TeX-PDF-mode t)
+    ;; 正向与反向搜索设置
+    (TeX-source-correlate-mode t)
+    (TeX-source-correlate-method 'synctex)
+    ;; 使用 eaf-pdf-viewer 打开PDF
+    (TeX-view-program-selection '((output-pdf "eaf")))
+    :config
+    (setq TeX-auto-save t)
+    ;; 编译时问询主文件名称
+    (setq-default TeX-master t)                               
+    ;; 编译后更新pdf文件
+    (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+    ;; 加载LaTeX模式设置
+    (add-hook 'LaTeX-mode-hook 'my-latex-hook)
+    ;; XeLaTeX 支持
+    (add-hook 'LaTeX-mode-hook (lambda()
+                                 (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+                                 (add-to-list 'TeX-view-program-list '("eaf" eaf-pdf-synctex-forward-view))
+                                 (setq TeX-save-query nil
+                                       TeX-show-compilation t))))
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'cdlatex)
